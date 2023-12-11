abb075f2771151d5ea3e54833a35004637cd5d97
hieuChiNguyen <hieu.nc1707bkhn@gmail.com>
2023-12-11 09:26:59 +0700

    fix call api login

--- lib/sign_in/sign_in.dart
+++ lib/sign_in/sign_in.dart
@@ -24,163 +24,175 @@ class _SignIn extends State<SignIn> {
     double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      appBar: AppBar(),
-      backgroundColor: const Color(0xfff1f1f1),
-      body: Container(
-        child: Form(
-          key: formKey,
-          child: Column(
-            children: [
-              Container(
-                width: screenWidth,
-                height: screenHeight * 0.35,
-                color: Colors.blue[700],
-                child: Icon(
-                  Icons.facebook_outlined,
-                  size: screenWidth * 0.2,
-                  color: Colors.white,
+        appBar: AppBar(),
+        backgroundColor: const Color(0xfff1f1f1),
+        body: SingleChildScrollView(
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                Container(
+                  width: screenWidth,
+                  height: screenHeight * 0.35,
+                  color: Colors.blue[700],
+                  child: Icon(
+                    Icons.facebook_outlined,
+                    size: screenWidth * 0.2,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-              Padding(
-                  padding: EdgeInsets.only(top: screenHeight * 0.03),
-                  child: const Text("Đăng nhập và tham gia cùng chúng tôi")),
-              Padding(
-                  padding: EdgeInsets.only(top: screenHeight * 0.03),
-                  child: Column(
-                    children: [
-                      TextFormField(
+                Padding(
+                    padding: EdgeInsets.only(top: screenHeight * 0.03),
+                    child: const Text("Đăng nhập và tham gia cùng chúng tôi")),
+                Padding(
+                    padding: EdgeInsets.only(top: screenHeight * 0.03),
+                    child: Column(
+                      children: [
+                        TextFormField(
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return "Chưa nhập địa chỉ email !";
+                              }
+                              if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
+                                  .hasMatch(value)) {
+                                return "Nhập địa chỉ email sai định dạng !";
+                              }
+                            },
+                            controller: _emailController,
+                            decoration:
+                                const InputDecoration(hintText: "Nhập email")),
+                        const Padding(padding: EdgeInsets.only(top: 20)),
+                        TextFormField(
                           validator: (value) {
-                            if (value!.isEmpty) {
-                              return "Chưa nhập địa chỉ email !";
-                            }
-                            if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
-                                .hasMatch(value)) {
-                              return "Nhập địa chỉ email sai định dạng !";
+                            if (value!.length < 6) {
+                              return "Mật khẩu cần ít nhất 6 kí tự !";
                             }
                           },
-                          controller: _emailController,
+                          obscureText: true,
+                          obscuringCharacter: "*",
+                          controller: _passwordController,
                           decoration:
-                              const InputDecoration(hintText: "Nhập email")),
-                      const Padding(padding: EdgeInsets.only(top: 20)),
-                      TextFormField(
-                        validator: (value) {
-                          if (value!.length < 6) {
-                            return "Mật khẩu cần ít nhất 6 kí tự !";
-                          }
-                        },
-                        obscureText: true,
-                        obscuringCharacter: "*",
-                        controller: _passwordController,
-                        decoration:
-                            const InputDecoration(hintText: "Nhập mật khẩu"),
-                      ),
-                      MaterialButton(
-                        onPressed: () {
-                          if (formKey.currentState!.validate()) {
-                            String email = _emailController.text;
-                            String password = _passwordController.text;
-
-                            signIn(email, password, 'uuid');
-
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => Home()),
-                            );
-                          } else {}
-                        },
-                        child: Container(
-                          margin: const EdgeInsets.only(top: 20),
-                          height: screenHeight * 0.06,
-                          width: screenWidth,
-                          color: Colors.blue[700],
-                          alignment: Alignment.center,
-                          child: const Text(
-                            "Đăng nhập",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold),
-                          ),
+                              const InputDecoration(hintText: "Nhập mật khẩu"),
                         ),
-                      ),
-                      MaterialButton(
-                        onPressed: () async {
-                          String email = _emailController.text;
-                          emailData = email;
-                          final response = await _getVerifyCodeResponse(email);
+                        MaterialButton(
+                          onPressed: () async {
+                            if (formKey.currentState!.validate()) {
+                              try {
+                                String email = _emailController.text;
+                                String password = _passwordController.text;
 
-                          if (response.statusCode == 200) {
-                            try {
-                              // Nếu không có lỗi, chuyển đổi nội dung JSON thành đối tượng Dart
-                              final jsonResponse = json.decode(response.data);
+                                final response =
+                                    await signIn(email, password, 'uuid');
 
-                              // Truy cập thuộc tính của đối tượng Dart
-                              verifyCodeData =
-                                  jsonResponse['data']['verify_code'];
-
-                              // In giá trị ra console
-                              print("Verify Code: $verifyCodeData");
-
-                              if (!context.mounted) return;
-                              _sendVerifyCode(context);
-                            } catch (e) {
-                              print("Error parsing JSON: $e");
+                                final jsonResponse = json.decode(response.data);
+                                String message = jsonResponse['message'];
+                                print(message);
+                                if (message == 'OK') {
+                                  if (!context.mounted) return;
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Home()),
+                                  );
+                                }
+                              } catch (e) {
+                                print("Đăng nhập không thành công");
+                              }
                             }
-                          } else {
-                            // Xử lý lỗi nếu có
-                            print("Lỗi khi lấy verify code");
-                          }
-                        },
-                        child: Padding(
-                          padding: EdgeInsets.only(top: screenHeight * 0.03),
-                          child: const Text(
-                            "Quên mật khẩu?",
-                            style: TextStyle(color: Colors.blue),
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.only(top: 20),
+                            height: screenHeight * 0.06,
+                            width: screenWidth,
+                            color: Colors.blue[700],
+                            alignment: Alignment.center,
+                            child: const Text(
+                              "Đăng nhập",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            ),
                           ),
                         ),
-                      ),
-                      Padding(
-                          padding: EdgeInsets.only(top: screenHeight * 0.03),
-                          child: const Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text("---------------------",
-                                  style: TextStyle(color: Colors.black)),
-                              Text(" OR ",
-                                  style: TextStyle(color: Colors.black)),
-                              Text("---------------------",
-                                  style: TextStyle(color: Colors.black)),
-                            ],
-                          )),
-                      MaterialButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const SignUpPage()));
-                        },
-                        child: Container(
-                          margin: const EdgeInsets.only(top: 20),
-                          height: screenHeight * 0.06,
-                          width: screenWidth,
-                          color: Colors.green[700],
-                          alignment: Alignment.center,
-                          child: const Text(
-                            "Tạo tài khoản mới",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold),
+                        MaterialButton(
+                          onPressed: () async {
+                            String email = _emailController.text;
+                            emailData = email;
+                            final response =
+                                await _getVerifyCodeResponse(email);
+
+                            if (response.statusCode == 200) {
+                              try {
+                                // Nếu không có lỗi, chuyển đổi nội dung JSON thành đối tượng Dart
+                                final jsonResponse = json.decode(response.data);
+
+                                // Truy cập thuộc tính của đối tượng Dart
+                                verifyCodeData =
+                                    jsonResponse['data']['verify_code'];
+
+                                // In giá trị ra console
+                                print("Verify Code: $verifyCodeData");
+
+                                if (!context.mounted) return;
+                                _sendVerifyCode(context);
+                              } catch (e) {
+                                print("Error parsing JSON: $e");
+                              }
+                            } else {
+                              // Xử lý lỗi nếu có
+                              print("Lỗi khi lấy verify code");
+                            }
+                          },
+                          child: Padding(
+                            padding: EdgeInsets.only(top: screenHeight * 0.03),
+                            child: const Text(
+                              "Quên mật khẩu?",
+                              style: TextStyle(color: Colors.blue),
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ))
-            ],
+                        Padding(
+                            padding: EdgeInsets.only(top: screenHeight * 0.03),
+                            child: const Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text("---------------------",
+                                    style: TextStyle(color: Colors.black)),
+                                Text(" OR ",
+                                    style: TextStyle(color: Colors.black)),
+                                Text("---------------------",
+                                    style: TextStyle(color: Colors.black)),
+                              ],
+                            )),
+                        MaterialButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const SignUpPage()));
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.only(top: 20),
+                            height: screenHeight * 0.06,
+                            width: screenWidth,
+                            color: Colors.green[700],
+                            alignment: Alignment.center,
+                            child: const Text(
+                              "Tạo tài khoản mới",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ))
+              ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 
   Future<Response> _getVerifyCodeResponse(String email) async {
